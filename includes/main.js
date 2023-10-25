016c06204c85bb7d7a6b8ff237a835329b1f8623
SaraiRaider <saraielizondomartinez@gmail.com>
2023-10-25 14:49:40 -0600

    Agregue cosas para probar las colisiones
    Solo que no logro ver al cubo2

--- includes/main.js
+++ includes/main.js
@@ -1,86 +1,93 @@
 import * as THREE from 'three';
-
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// Variables para el proceso de colisiones
+var objModelos3d;
+let Modelos3dBB;
+Modelos3dBB = new THREE.Box3().setFromObject(objModelos3d);
+var previousModelPosition = new THREE.Vector3();
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// manager
-
+// Manager
 const manager = new THREE.LoadingManager();
-manager.onStart = function ( url, itemsLoaded, itemsTotal ) {
-    console.log( 'Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
-};
- 
-manager.onLoad = function ( ) {
-    console.log( 'Loading complete!');
-};
- 
-manager.onProgress = function ( url, itemsLoaded, itemsTotal ) {
-    console.log( 'Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
-};
- 
-manager.onError = function ( url ) {
-    console.log( 'There was an error loading ' + url );
+manager.onStart = function (url, itemsLoaded, itemsTotal) {
+    console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
 };
 
-const controls = new OrbitControls( camera, renderer.domElement );
+manager.onLoad = function () {
+    console.log('Loading complete!');
+};
+
+manager.onProgress = function (url, itemsLoaded, itemsTotal) {
+    console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
+};
+
+manager.onError = function (url) {
+    console.log('There was an error loading ' + url);
+};
+
+const controls = new OrbitControls(camera, renderer.domElement);
 const loader = new GLTFLoader();
 
-//directional 
+// Crear un cubo para colisiones
+const geometry_cube2 = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+const material_cube2 = new THREE.MeshPhongMaterial({ color: 0x874141 });
+cube2.scale.set(2, 2, 2);
 
-const light = new THREE.DirectionalLight( 0xFFFFFF );
+const cube2 = new THREE.Mesh(geometry_cube2, material_cube2);
+cube2.position.x = 3;
+cube2.position.y = 0;
+cube2.position.z = 0;
+
+var cube2BB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+cube2BB.setFromObject(cube2);
+scene.add(cube2BB);
+scene.add(cube2);
+
+
+// Direccional
+const light = new THREE.DirectionalLight(0xFFFFFF);
 light.position.set(1, 2, 3);
-//const helper = new THREE.DirectionalLightHelper( light,100 );
-scene.add(light );
+scene.add(light);
 
-const light1 = new THREE.DirectionalLight( 0xFFFFFF );
+const light1 = new THREE.DirectionalLight(0xFFFFFF);
 light1.position.set(0, 3, 0);
-//const helper1 = new THREE.DirectionalLightHelper( light1,100 );
-scene.add(light1 );
+scene.add(light1);
 
-const light2 = new THREE.DirectionalLight( 0xFFFFFF );
+const light2 = new THREE.DirectionalLight(0xFFFFFF);
 light2.position.set(1, 5, -30);
-//const helper2 = new THREE.DirectionalLightHelper( light2,100 );
-scene.add(light2 );
+scene.add(light2);
 
-const light3 = new THREE.DirectionalLight( 0xFFFFFF );
-light3.position.set(-5,0,0);
-//const helper3 = new THREE.DirectionalLightHelper( light3,100 );
+const light3 = new THREE.DirectionalLight(0xFFFFFF);
+light3.position.set(-5, 0, 0);
 scene.add(light3);
 
-/*const pointLight = new THREE.PointLight( 0xff0000, 1, 100 );
-pointLight.position.set( 10, 10, 10 );
-scene.add( pointLight );*/
+// Cubos
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-/*const sphereSize = 1;
-const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
-scene.add( pointLightHelper );*/
+const geometry_2 = new THREE.BoxGeometry(1, 1, 1);
+const material_2 = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+const cube_2 = new THREE.Mesh(geometry_2, material_2);
+scene.add(cube_2);
 
-// agregar cubos
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-const geometry_2 = new THREE.BoxGeometry( 1, 1, 1 );
-const material_2 = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
-const cube_2 = new THREE.Mesh( geometry_2, material_2 );
-scene.add( cube_2 );
 
 scene.background = new THREE.Color(0x15A4CE);
 
 camera.position.z = 5;
-//cube.position.x= 1;
-cube_2.position.x=5;
+cube_2.position.x = 5;
+///////////////////////
 
 Modelos3D();
 Barco2();
@@ -88,32 +95,85 @@ Barco3();
 Barco4();
 Barco5();
 
-function animate() {
-	requestAnimationFrame( animate );
-
-	//cube.rotation.x += 0.01;
-	//cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
+function detectCollision() {
+    if (Modelos3dBB.intersectsBox(cube2BB)) {
+        console.log('Colisión detectada entre Modelos3D y cube2');
+        // Realiza otras acciones si es necesario
+    }
+    // Realiza verificaciones similares para otros objetos en tu escena
 }
 
-//***************************************//
-// REPRESENTAR POSICION ACTUAL DE Modelos3D//
+function init() {
 
+    // Escuchar eventos de teclado para mover "cube2"
+    document.addEventListener('keydown', (event) => {
+        switch (event.key) {
+            case 'e':
+                cube2.position.z -= 0.1; // Mover hacia adelante
+                break;
+            case 'r':
+                cube2.position.z += 0.1; // Mover hacia atrás
+                break;
+            case 'd':
+                cube2.position.x -= 0.1; // Mover hacia la izquierda
+                break;
+            case 'f':
+                cube2.position.x += 0.1; // Mover hacia la derecha
+                break;
+        }
+        // Actualizar la posición de "cube2"
+        cube2BB.setFromObject(cube2);
+        // Luego, puedes llamar a detectCollision() para verificar colisiones
+        detectCollision();
+    });
+}
+
+    
+function animate() {
+   
+        requestAnimationFrame(animate);
+        // Actualiza la posición de cube2 para que siga al modelo
+        cube2.position.copy(objModelos3d.position);
+    
+        // Verifica la colisión
+        detectCollision();
+    requestAnimationFrame(animate);
+
+    // Actualiza la posición de cube2 para que siga al modelo
+    cube2.position.copy(objModelos3d.position);
+
+    // Verifica la colisión
+    if (Modelos3dBB.intersectsBox(cube2BB)) {
+        // Colisión detectada con 'cube2'
+        // Realiza acciones o maneja la colisión aquí
+        // Por ejemplo, puedes detener el movimiento del modelo
+        // objModelos3d.position.copy(previousModelPosition);
+    }
+
+    // Resto del código de animación
+    // ...
+
+    renderer.render(scene, camera);
+}
+
+// Representar posición actual de Modelos3D
 const modelos3DPosition = {
-    x: 1,
-    y: 1,
+    x: 3,
+    y: 0,
     z: 0
 };
-// REPRESENTAR POSICION ACTUAL DEL BARCO2//
+
+// Representar posición actual de Barco2
 const barco2Position = {
     x: 3,
     y: 0,
     z: 0
 };
-//***************************************//
+
+
 
 animate();
+function init();
 
 function Modelos3D() {
     const loaderBarco = new OBJLoader(manager);
@@ -146,6 +206,8 @@ function Modelos3D() {
                 }
                 // Actualizar la posición del modelo
                 object.position.set(modelos3DPosition.x, modelos3DPosition.y, modelos3DPosition.z);
+                Modelos3dBB.setFromObject(objModelos3d); // Actualiza el BoundingBox del modelo
+                Modelos3dBB = new THREE.Box3().setFromObject(objModelos3d);
             });
         });
         console.log(materials);
